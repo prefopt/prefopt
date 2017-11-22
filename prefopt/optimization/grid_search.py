4f0e101fb43888d71b985119be66c50d8bf73e83
Jakob Bauer <jakobworldpeace@bluewin.ch>
2017-11-21 20:03:03 -0500

    Add grid search optimizer

---
+++ prefopt/optimization/grid_search.py
@@ -0,0 +1,83 @@
+"""
+Grid search optimizer.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from prefopt.acquisition import Optimizer
+
+import itertools
+
+import numpy as np
+
+__all__ = ['GridSearchOptimizer']
+
+
+def compute_grid(bounds, n_gridpoints, dtype=np.float32):
+    """
+    Compute grid points.
+
+    Parameters
+    ----------
+    bounds : list
+        List of bounding boxes of the form (start, end) for each dimension of
+        the domain.
+    n_gridpoints : float
+        Approximate number of grid points.
+    dtype : numpy.dtype, optional (default: np.float32)
+        The data type of the grid array.
+
+    Returns
+    -------
+    grid : numpy.array
+        Numpy array of grid point tuples.
+
+    Examples
+    --------
+    >>> bounds = [[0, 10], [0, 10]]
+    >>> grid = compute_grid(bounds, 1000)
+    >>> len(grid)
+    1024
+    >>> grid[:3]  # doctest: +NORMALIZE_WHITESPACE
+    array([[ 0.        ,  0.        ],
+           [ 0.        ,  0.32258064],
+           [ 0.        ,  0.64516127]], dtype=float32)
+    >>> grid[-3:]  # doctest: +NORMALIZE_WHITESPACE
+    array([[ 10.        ,   9.35483837],
+           [ 10.        ,   9.67741966],
+           [ 10.        ,  10.        ]], dtype=float32)
+
+    """
+    # compute the exact number of grid points
+    n_dim = len(bounds)
+    n_gridpoints_exact = np.round(np.power(n_gridpoints, 1.0 / n_dim))
+
+    # compute gridpoints
+    ticks = [np.linspace(*x, num=n_gridpoints_exact) for x in bounds]
+    grid = list(itertools.product(*ticks))
+    return np.array(grid, dtype=dtype)
+
+
+class GridSearchOptimizer(Optimizer):
+    """
+    Grid search optimizer.
+
+    Parameters
+    ----------
+    bounds : array-like
+        Bounds of the form (lower, upper) for each dimension of x.
+    n_gridpoints : int, optional (default: 1000000)
+        Approximate number of grid points.
+    """
+
+    def __init__(self, bounds, n_gridpoints=1000000):
+        self.bounds = bounds
+        self.n_gridpoints = n_gridpoints
+
+    def maximize(self, func):
+        grid = compute_grid(self.bounds, self.n_gridpoints)
+        f = func(grid)
+        ix = np.argmax(f)
+        return grid[ix]
---
+++ tests/optimization/test_grid_search.py
@@ -0,0 +1,58 @@
+"""
+Tests for prefopt.optimization.grid_search.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+
+import numpy as np
+
+from prefopt.optimization.grid_search import GridSearchOptimizer, compute_grid
+
+
+class TestHelperFunctions(unittest.TestCase):
+
+    def test_compute_grid(self):
+        bounds = [(0, 10), (-5, 1)]
+        n_gridpoints = 1000
+        dtype = np.float32
+        grid = compute_grid(bounds, n_gridpoints, dtype=dtype)
+        n_gridpoints_exact = np.sqrt(len(grid))
+        ticks = [np.linspace(*x, num=n_gridpoints_exact) for x in bounds]
+        true_grid = np.array(
+            zip(*[x.flatten() for x in np.meshgrid(*ticks, indexing='ij')]),
+            dtype=dtype
+        )
+        self.assertEqual(true_grid.shape, grid.shape)
+        self.assertTrue(np.allclose(grid, true_grid))
+
+
+class TestOptimization(unittest.TestCase):
+
+    def test_one_dimensional(self):
+        bounds = [(-9, 10)]
+        optimizer = GridSearchOptimizer(bounds)
+
+        # linear
+        x = optimizer.maximize(lambda x: x)
+        self.assertTrue(np.isclose(x, 10))
+
+        # linear
+        x = optimizer.maximize(lambda x: -x)
+        self.assertTrue(np.isclose(x, -9))
+
+        # quadratic
+        x = optimizer.maximize(lambda x: pow(x, 2)),
+        self.assertTrue(np.isclose(x, 10))
+
+        # quadratic
+        # NOTE absolute tolerance needs to be increased for this test to pass
+        x = optimizer.maximize(lambda x: -pow(x, 2)),
+        self.assertTrue(np.isclose(x, 0, atol=1e-5), x)
+
+
+if __name__ == '__main__':
+    unittest.main()
