09f5af39f6feac0b0aad392a99cf0b059eb4dc0d
Jakob Bauer <jakobworldpeace@bluewin.ch>
2018-02-16 18:58:32 -0500

    Add better isolation for experiments

--- features/steps/experiment.py
+++ features/steps/experiment.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import multiprocessing
+
 import prefopt.experiment
 import prefopt.presenter
 import prefopt.utility
@@ -39,23 +41,31 @@ def step_impl(context, utility_name, lower, upper, acquisition_strategy,
 def step_impl(context, number_of_runs, number_of_iterations):
     context.results = []
     for _ in range(number_of_runs):
-        acquirer = prefopt.experiment.create_acquirer(
-            context.acquisition_strategy,
-            context.model_name,
-            context.optimizer_name,
-            context.bounds
-        )
-        output_presenter = prefopt.presenter.StdoutPresenter()
-        experiment = prefopt.experiment.PreferenceExperiment(
-            acquirer,
-            context.input_presenter,
-            output_presenter,
-            context.seed_data
-        )
-        for _ in range(number_of_iterations):
-            experiment.run()
-        best, = experiment.acquirer.best
-        context.results.append(best)
+        q = multiprocessing.Queue()
+
+        def func(q):
+            acquirer = prefopt.experiment.create_acquirer(
+                context.acquisition_strategy,
+                context.model_name,
+                context.optimizer_name,
+                context.bounds
+            )
+            output_presenter = prefopt.presenter.StdoutPresenter()
+            experiment = prefopt.experiment.PreferenceExperiment(
+                acquirer,
+                context.input_presenter,
+                output_presenter,
+                context.seed_data
+            )
+            for _ in range(number_of_iterations):
+                experiment.run()
+            best, = experiment.acquirer.best
+            q.put(best)
+
+        p = multiprocessing.Process(target=func, args=(q,))
+        p.start()
+        context.results.append(q.get())
+        p.join()
 
 
 @then('the result should be within {epsilon:e} of the optimum'
